8f0c7420b14e6a19403df73d56771a6ad865c496
tomokoau <97634674+tomokoau@users.noreply.github.com>
2023-01-31 11:54:29 -1000

    Update Color recognition.py

--- Color recognition.py
+++ Color recognition.py
@@ -1,9 +1,21 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+
 import cv2
 from sklearn.cluster import KMeans
+img = cv2.imread("test2.png") 
+img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+img = img.reshape((img.shape[1]*img.shape[0],3))
 
+# import the modules
+import os
+from os import listdir
+ 
+# get the path/directory
+folder_dir = "C:/Users/RIJUSHREE/Desktop/Gfg images"
+for images in os.listdir(folder_dir):
+ 
+    # check if the image ends with png
+    if (images.endswith(".png")):
+        print(images)
 
 
 # kmeans code 
@@ -27,7 +39,7 @@ print(percent)
 
 
 
-kmeans=KMeans(n_clusters=4)
+kmeans=KMeans(n_clusters=7)
 s=kmeans.fit(img)
 labels=kmeans.labels_
 centroid=kmeans.cluster_centers_
