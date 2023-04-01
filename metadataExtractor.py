cca79454d9abb9a4f9dec7e2bf05d09f1778f1ee
Tonyl73 <124114090+Tonyl73@users.noreply.github.com>
2023-04-01 01:01:28 -1000

    Update metadataExtractor.py
    Extracts pitch, yaw, roll, latitude, and longitude.

--- metadataExtractor.py
+++ metadataExtractor.py
@@ -1,5 +1,30 @@
 from exiftool import ExifToolHelper
+
+pitch = 0
+yaw = 0
+roll = 0
+latitude = 0
+longitude = 0
+
 with ExifToolHelper() as et:
     for d in et.get_metadata("real2.jpg"):
         for k, v in d.items():
-            print(f"Dict: {k} = {v}")
+            #read latitude
+            if (k == "EXIF:GPSLatitude"):
+                latitude = float(v)
+            #read longitude
+            if (k == "EXIF:GPSLongitude"):
+                longitude = float(v)
+            #read comment line containing pitch,yaw,roll
+            if (k == "File:Comment"):
+                #split up pitch,yaw,roll
+                p_y_r = v.split(" ")
+                #create empty array to hold value pairs
+                p_y_r_vals = []
+                #split up the three values into proprty and number
+                for val in p_y_r:
+                    p_y_r_vals.append(val.split(":"))
+                #take the numbers from each property and assign to variables
+                pitch = float(p_y_r_vals[0][1])
+                yaw = float(p_y_r_vals[1][1])
+                roll = float(p_y_r_vals[2][1])
